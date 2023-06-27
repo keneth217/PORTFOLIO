0f2770492ba9de8d8f867a239236c926be04ae9d
keneth217 <kipyegonkeneth03@gmail.com>
2023-06-27 13:58:13 +0300

    port home

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const NavBar = () => {
+function NavBar() {
+  const [links, setLinks] = useState([
+    { name: "home", link: "/" },
+    { name: "about", link: "/about" },
+    { name: "projects", link: "/projects" },
+    { name: "contact us", link: "/contact" },
+    {
+      name: "others",
+      link: "/others",
+      subItems: [
+        { name: "Others 1", link: "/others1" },
+        { name: "Others 2", link: "/others2" },
+        { name: "Others 3", link: "/others3" },
+      ],
+    },
+  ]);
+
   return (
-    <div>
-      
-    </div>
-  )
+    <Navbar expand="lg" className="bg-body-tertiary" fixed="top">
+      <Container>
+        <Navbar.Brand href="#home">KENETH</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto text uppercase">
+            {links.map((link) => (
+              <React.Fragment key={link.name}>
+                <Nav.Link href={link.link}>{link.name}</Nav.Link>
+                {link.subItems && (
+                  <NavDropdown title={link.name} id="basic-nav-dropdown">
+                    {link.subItems.map((subItem) => (
+                      <NavDropdown.Item key={subItem.name} href={subItem.link}>
+                        {subItem.name}
+                      </NavDropdown.Item>
+                    ))}
+                  </NavDropdown>
+                )}
+              </React.Fragment>
+            ))}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default NavBar
+export default NavBar;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./Home.css";
 
-const Home = () => {
+function Home() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Container className='vh-100 bg-dark'>
+      <Row className="p-5">
+        <Col sm={8}>about me</Col>
+        <Col sm={4}>
+          <div className="container">
+            <div id="cube">
+              <div className="face front">1</div>
+              <div className="face back">2</div>
+              <div className="face right">3</div>
+              <div className="face left">4</div>
+              <div className="face top">5</div>
+              <div className="face bottom">6</div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default Home
+export default Home;
