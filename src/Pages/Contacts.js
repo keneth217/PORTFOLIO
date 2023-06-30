704457fc5a0e96927d913ac5d7904e838bdb2550
keneth217 <kipyegonkeneth03@gmail.com>
2023-06-30 09:17:00 +0300

    pages

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -7,10 +7,10 @@ import Col from 'react-bootstrap/Col';
 import "./Home.css";
 function Contacts() {
   return (
-    <Container fluid className="bg-dark vh-100 d-flex align-items-center justify-content-center">
+    <Container fluid className="bg-dark vh-100 d-flex text-white align-items-center justify-content-center">
       <Row className="justify-content-center">
         <Col xs={12} md={6}>
-          <Card className="text-center bg-dark text-white" style={{ width: '50vw' }}>
+          <Card className="text-center bg-dark text-white " style={{ width: '50vw' ,height:'50vh'}}>
             <Card.Header>contact me</Card.Header>
             <Card.Body>
               <Card.Title>fill the form below</Card.Title>
@@ -18,21 +18,21 @@ function Contacts() {
                 <Form>
                   <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control type="email" placeholder="name@example.com" />
+                    <Form.Control type="text" placeholder="Enter your name" />
                   </Form.Group>
                   <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                     <Form.Label>Email</Form.Label>
                     <Form.Control type="email" placeholder="name@example.com" />
                   </Form.Group>
-                  <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                  <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" >
                     <Form.Label>Message</Form.Label>
-                    <Form.Control as="textarea" rows={3} />
+                    <Form.Control as="textarea" rows={3} placeholder="Type your message here"/>
                   </Form.Group>
                 </Form>
               </Card.Text>
               <Button variant="primary">submit</Button>
             </Card.Body>
-            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+            {/* <Card.Footer className="text-italic text-white">2 days ago</Card.Footer> */}
           </Card>
         </Col>
       </Row>
