e9209ed6440f1d155dcec5fd1f90629c268a92bc
keneth217 <kipyegonkeneth03@gmail.com>
2023-07-03 17:59:41 +0300

    commit

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -15,7 +15,7 @@ function NavBar() {
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 text-uppercase">
         <li class="nav-item">
          < Link class="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
